4ca6bd96771bbac59b146ea175cae55ab56b5ed4
taoguangc <taoguangc@gmail.com>
2023-10-06 11:16:58 +0800

    add output: 'export' to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
-  swcMinify: true,
-};
+  swcMinify: true
+}
 
-module.exports = withContentlayer(nextConfig);
+module.exports = withContentlayer(nextConfig)
