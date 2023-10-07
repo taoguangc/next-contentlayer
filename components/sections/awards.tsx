ad034f8eadf096a153bbd789770ee3523c64352b
taoguangc <taoguangc@gmail.com>
2023-10-07 19:02:15 +0800

    update awards component class

--- components/sections/awards.tsx
+++ components/sections/awards.tsx
@@ -3,7 +3,7 @@ export default function Awards() {
     <section className="py-40">
       <div className="container mx-auto flex flex-row flex-wrap gap-y-8 px-4 lg:gap-y-16 lg:px-8">
         <div className="flex w-1/2 flex-row items-end xl:w-1/4">
-          <h2 className="stroke mr-2 text-5xl font-extrabold tracking-tighter md:text-7xl xl:text-8xl">15</h2>
+          <h2 className="stroke-text text-white mr-2 text-5xl font-extrabold tracking-tighter md:text-7xl xl:text-8xl">15</h2>
           <h3 className="text-xl">年经验</h3>
         </div>
         <div className="flex w-1/2 flex-row items-end xl:w-1/4">
@@ -11,7 +11,7 @@ export default function Awards() {
           <h3 className="text-xl">个项目</h3>
         </div>
         <div className="flex w-1/2 flex-row items-end xl:w-1/4">
-          <h2 className="stroke mr-2 text-5xl font-extrabold tracking-tighter md:text-7xl xl:text-8xl">100</h2>
+          <h2 className="stroke-text text-white mr-2 text-5xl font-extrabold tracking-tighter md:text-7xl xl:text-8xl">100</h2>
           <h3 className="text-xl">个产品</h3>
         </div>
         <div className="flex w-1/2 flex-row items-end xl:w-1/4">
