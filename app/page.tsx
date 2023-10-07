bee29cb2c82b76b8cca0c986ac57389caa6d2067
taoguangc <taoguangc@gmail.com>
2023-10-07 19:37:39 +0800

    add themes to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import { allPosts } from 'contentlayer/generated'
 import Link from 'next/link'
 import Hero from '@/components/sections/hero'
-import Services from '@/components/sections/services'
 import Awards from '@/components/sections/awards'
+import Themes from '@/components/sections/themes'
+import Services from '@/components/sections/services'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Awards />
+      <Themes />
       <Services />
       <div className='mx-auto container prose'>
       {allPosts.map(post => (
