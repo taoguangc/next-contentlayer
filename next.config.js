bc59d1baeddc50f743c25e27ab8dcc525d428e41
taoguangc <taoguangc@gmail.com>
2023-10-08 16:57:14 +0800

    remove output: 'export',

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   images: { unoptimized: true },
   reactStrictMode: true,
   swcMinify: true
